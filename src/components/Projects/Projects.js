49dcd4c8411db19b644c82925a357c015bce48d9
Sagar Giradkar <108196738+sagargiradkar@users.noreply.github.com>
2025-01-22 16:02:56 +0530

    projects updated

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -2,13 +2,9 @@ import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
-import leaf from "../../Assets/Projects/leaf.png";
-import emotion from "../../Assets/Projects/emotion.png";
-import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
-import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
-
+import razorpay from "../../Assets/Projects/project1.png";
+import poshinda from "../../Assets/Projects/project2.png"
+import hotstar from "../../Assets/Projects/project3.png"
 function Projects() {
   return (
     <Container fluid className="project-section">
@@ -23,12 +19,32 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={razorpay}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/sagargiradkar"
-              demoLink="https://49.web.app/"
+              title="Razorpay Clone Website UI"
+              description="Razorpay is the only payments solution in India that allows businesses to accept, process and disburse payments with its product suite."
+              ghLink="https://github.com/sagargiradkar/Razorpay-Clone"
+              demoLink="https://razorpayclonesagar.netlify.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={poshinda}
+              isBlog={false}
+              title="Poshinda.co"
+              description="Welcome to Poshinda.co, an agriculture product trading company founded by four dedicated farmer sons. We are committed to bringing the pure essence of nature and soil directly to you. Our journey started in the fertile lands where we learned the value of hard work, dedication, and sustainable farming practices."
+              ghLink="https://github.com/sagargiradkar/shopping-cart"
+              demoLink="https://shopping-cart-tan-delta.vercel.app/"
+            />
+          </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={hotstar}
+              isBlog={false}
+              title="Hotstar Website Clone Website UI"
+              description="Disney + Hotstar is an Indian subscription video on-demand over-the-top streaming service owned by The Walt Disney Company India and operated by Disney Streaming ."
+              ghLink="https://github.com/sagargiradkar/Disney-Hostar-website"
+              demoLink=""
             />
           </Col>
         </Row>
